0320fcddc5587b253ca1c9edd514a8d676136eb2
Brandon Duong <bduong1497@gmail.com>
2024-07-03 16:29:05 -0400

    Express REST API with MongoDB and Mongoose Part 2

--- app.js
+++ app.js
@@ -1,16 +1,15 @@
 const createError = require("http-errors");
 const express = require("express");
-const path = require("path");
 const cookieParser = require("cookie-parser");
 const logger = require("morgan");
+const path = require("path");
+const mongoose = require("mongoose");
 
 const indexRouter = require("./routes/index");
 const usersRouter = require("./routes/users");
 const campsiteRouter = require("./routes/campsiteRouter");
 const promotionRouter = require("./routes/promotionRouter");
 const partnerRouter = require("./routes/partnerRouter");
-
-const mongoose = require("mongoose");
 
 const url = "mongodb://localhost:27017/nucampsite";
 const connect = mongoose.connect(url, {});
